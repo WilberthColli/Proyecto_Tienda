3d8517d10f07f8379dd60ec7dc8a8fe31c597028
WilberthColli <manuelcolli05@gmail.com>
2022-10-22 20:47:00 -0500

    Añadida la clase repository para producots

--- TiendaJoya/src/main/java/com/example/TiendaJoya/TiendaJoyaApplication.java
+++ TiendaJoya/src/main/java/com/example/TiendaJoya/TiendaJoyaApplication.java
@@ -1,13 +1,35 @@
 package com.example.TiendaJoya;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.TiendaJoya.repository.ProductosRepository;
+
 @SpringBootApplication
-public class TiendaJoyaApplication {
+public class TiendaJoyaApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(TiendaJoyaApplication.class, args);
+	}
+	
+	@Autowired
+	private ProductosRepository repoProductos;
+	
+	private void guardar() {
+		System.out.println("insertando un nuevo registro");
+	}
+	
+	private void eliminar() {
+		System.out.println("insertando un nuevo registro");
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println(repoProductos);
+		
 	}
 
 }
---
+++ TiendaJoya/src/main/java/com/example/TiendaJoya/repository/ProductosRepository.java
@@ -0,0 +1,9 @@
+package com.example.TiendaJoya.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.TiendaJoya.model.Productos;
+
+public interface ProductosRepository extends CrudRepository<Productos, Integer> {
+
+}
